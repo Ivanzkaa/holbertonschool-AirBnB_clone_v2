b867400e404efdbdf2211dded3427db75f70f0a5
Ivanzkaa <ivanzka@me.com>
2022-11-30 10:50:10 -0400

    updating the route with the text replacement

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""a flask web application"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
+@app.route('/c/<text>', strict_slashes=False)
+
+def text():
+    """displaying c_text"""
+    return 'C %s' % text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
