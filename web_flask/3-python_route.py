0861117e497882e6060d96f6646e3cbe477e4638
Ivanzkaa <ivanzka@me.com>
2022-12-01 13:10:34 -0400

    adding the python text

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""a flask web application"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def Hello_HBNB():
+    """displaying a message"""
+    return ('Hello HBNB!')
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """displaying hbnb"""
+    return ('HBNB')
+@app.route('/c/<text>', strict_slashes=False)
+
+def text():
+    """displaying c_text"""
+    return 'C %s' % text.replace('_', ' ')
+
+@app.route('/python/<text>', strict_slashes=False)
+def py_text(text="is_cool"):
+    """displaying a python text"""
+    text = text.replace('_', ' ')
+    return 'Python {}'. format(text)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
